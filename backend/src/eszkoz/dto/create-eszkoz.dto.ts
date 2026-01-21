4d3102055e9fddc7f3034ada7b30a2ece831ea2d
Szabó-Lövey Gergő <szabolovey.gergo@gmail.com>
2026-01-21 12:23:47 +0100

    validaciok kiegeszitve

--- backend/src/eszkoz/dto/create-eszkoz.dto.ts
+++ backend/src/eszkoz/dto/create-eszkoz.dto.ts
@@ -1,17 +1,21 @@
-import { IsBoolean, IsNumber, IsString } from "class-validator";
+import { IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class CreateEszkozDto {
 
        @IsString()
+       @IsNotEmpty()
        nev: string;
 
        @IsString()
+       @IsNotEmpty()
        tipus: string;
 
        @IsNumber()
+       @IsNotEmpty()
        darabszam: number;
 
        @IsBoolean()
+       @IsOptional()
        hasznalatban: boolean;
 
 }
--- backend/src/feladat/dto/create-feladat.dto.ts
+++ backend/src/feladat/dto/create-feladat.dto.ts
@@ -1,16 +1,20 @@
-import { IsBoolean, IsNumber, IsString } from "class-validator";
+import { IsBoolean, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class CreateFeladatDto {
 
        @IsNumber()
+       @IsNotEmpty()
        munka_id: number;
 
        @IsString()
+       @IsNotEmpty()
        leiras: string;
 
        @IsBoolean()
+       @IsOptional()
        isCompleted: boolean;
 
        @IsBoolean()
+       @IsOptional()
        isActive: boolean;
 }
