45a462bac847d63c97e6e022ec0bc4f02b7e19e7
Szabó-Lövey Gergő <szabolovey.gergo@gmail.com>
2026-01-21 14:11:55 +0100

    feladat, eszkoz, user interfacek kesz

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -2,4 +2,4 @@ export function Menu() {
        return <>
        
        </>
-}
+}   
---
+++ frontend/src/interfaces/Eszkoz.ts
@@ -0,0 +1,7 @@
+export interface Eszkoz {
+       eszkoz_id: string;
+       nev: string;
+       tipus: string;
+       darabszam: number;
+       hasznalatban: boolean;
+}
